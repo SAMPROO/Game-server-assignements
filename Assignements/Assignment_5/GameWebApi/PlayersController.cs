9f53ef29c39bdff90753f05c3f933f22a3ed8371
Iiro <pelttarii@gmail.com>
2019-10-08 22:46:01 +0300

    Ass 6 teht 1-2

--- Assignements/Assignment_5/GameWebApi/PlayersController.cs
+++ Assignements/Assignment_5/GameWebApi/PlayersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -15,23 +16,71 @@ namespace dotnetKole
             _repository = i;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:guid}")]
         public Task<Player> Get(Guid id)
-        {
+        {   
             return _repository.Get(id);
+          
+        }
+        [HttpGet("{name}")]
+        public Task<Player> Get(string name)
+        {
+            Console.WriteLine("heil afeasfasf "+name);
+            return Task.Run( () => {
+                    var players = _repository.GetAll().Result;
+                    foreach(var player in players)
+                    {
+                        if(player.Name==name)
+                        {
+                            return player;
+                        }
+                    }
+                    return null;
+                    } );
         }
         [HttpGet]
-        public Task<Player[]> GetAll()
+        public Task<Player[]> GetAll([FromQuery]int minScore = 0)
         {
+        
+            if(minScore>0)
+            { 
+                return Task.Run( () => {
+                    var players = _repository.GetAll().Result;
+                    var playersOver = new List<Player>();
+                    foreach(var player in players)
+                    {
+                        if(player.Score>=minScore)
+                        {
+                            playersOver.Add(player);
+                        }
+                    }
+                    return playersOver.ToArray();
+                    } );
+            }
+            else
+            {
+                return _repository.GetAll();
+            }
+            
+        }
+        /*
+        public Task<Player[]> GetAllMin([FromQuery]int scoreMin)
+        {
+            if(scoreMin>0)
+            {
+                Console.WriteLine("---------------------------All above "+scoreMin);
+            }
             return _repository.GetAll();
         }
+        */
         [HttpPost]
         public Task<Player> Create([FromBody]NewPlayer player)
         {
             return _repository.Create(player);
         }
 
-        public Task<Player> Modify(Guid id, ModifiedPlayer player)
+        [HttpPut("{id}")]
+        public Task<Player> Modify(Guid id,[FromBody]ModifiedPlayer player)
         {
             return _repository.Modify(id, player);
         }
